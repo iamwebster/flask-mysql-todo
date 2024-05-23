cedd035b04e7c67938cc56d4d1372430e88a44b1
Denis <kopyloveden@gmail.com>
2024-05-23 15:26:58 +0500

    initial commit

---
+++ create_db.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from dotenv import load_dotenv 
+import os 
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    user=os.getenv('MYSQL_USER'),
+    passwd=os.getenv('MYSQL_PASSWORD')
+)
+
+
+my_cursor = mydb.cursor()
+
+# my_cursor.execute("CREATE DATABASE opda")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
